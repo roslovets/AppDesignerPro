fe31b52e99611ec65fba539f183eaa57668c1c8c
Pavel Roslovets <roslovets@exponenta.ru>
2019-08-15 21:18:54 +0300

    uiinput for several fields and with optional show

--- +UI/Input.m
+++ +UI/Input.m
@@ -4,92 +4,120 @@ classdef Input < handle
     
     properties
         UIFigure
-        UIInput
-        Body
-        Value = ''
-        Height = 95
-        Width = 200
+        UIOverlay
+        UIFields
+        Fields
+        Values
+        Transparent
+        Width
+        Height
+        FieldsGap = 50
         BtnWidth = 75
-        BackgroundColor = [0.651 0.651 0.651]
-        FontSize = 12
-        Message = 'Enter value'
         Ok = false
     end
     
     methods
-        function obj = Input(uifig, msg, value)
+        function obj = Input(varargin)
             %% Constructor
-            if nargin > 0
-                obj.UIFigure = uifig;
-            else
+            p = inputParser();
+            p.addOptional('uifig', []);
+            p.addOptional('titles', 'Enter value', @(s)isstring(s)||ischar(s)||iscellstr(s));
+            p.addOptional('values', '', @(s)isstring(s)||ischar(s)||iscellstr(s));
+            p.addParameter('Width', 200);
+            p.addParameter('Transparent', false);
+            p.addParameter('Show', true);
+            p.parse(varargin{:});
+            args = p.Results;
+            if isempty(args.uifig)
                 obj.UIFigure = uifigure;
+            else
+                obj.UIFigure = args.uifig;
             end
-            if nargin > 1
-                obj.Message = msg;
+            obj.Width = args.Width;
+            obj.Transparent = args.Transparent;
+            obj.Fields = cell2table(cell(0, 2), 'VariableNames', {'title' 'value'});
+            obj.addFields(args.titles, args.values);
+            if args.Show
+                obj.show();
             end
-            if nargin > 2
-                obj.Value = value;
-            end
-            obj.initBody();
         end
-        
         
         function show(obj)
             %% Show input
-%             obj.Body.redraw();
-            obj.Body.show();
+            obj.UIOverlay.show();
             uiwait(obj.UIFigure);
+        end
+        
+        function addFields(obj, labels, values)
+            %% Add field to input form
+            labels = cellstr(labels);
+            values = cellstr(values);
+            obj.Fields = [obj.Fields; [labels(:) values(:)]];
+            obj.redraw();
         end
         
         function close(obj, varargin)
             %% Close input
-            obj.Body.hide()
+            obj.UIOverlay.hide()
             uiresume(obj.UIFigure);
         end
         
-        function apply(obj, inputObj)
+        function apply(obj)
             %% Apply and close input
-            obj.Value = inputObj.Value;
+            obj.Values = get(obj.UIFields, 'Value');
             obj.Ok = true;
             obj.close();
         end
         
-        function initBody(obj)
+        function redraw(obj)
             %% Initialize Body
-            if ~isempty(obj.Body) && isvalid(obj.Body)
-                delete(obj.Body);
+            if ~isempty(obj.UIOverlay) && isvalid(obj.UIOverlay)
+                delete(obj.UIOverlay);
             end
-            obj.Body = UI.Overlay(obj.UIFigure, 'Width', obj.Width,...
-                'Height', obj.Height, 'BackgroundColor', obj.BackgroundColor);
-            panel = obj.Body.UIPanel;
-            refpos = [0 0 panel.Position([3 4])];
-            obj.addField(panel, obj.Message, obj.Value);
+            obj.Height = 40 + height(obj.Fields) * obj.FieldsGap;
+            if obj.Transparent
+                args = {'BackgroundColor', 'none'};
+            else
+                args = {};
+            end
+            obj.UIOverlay = UI.Overlay(obj.UIFigure, 'Width', obj.Width,...
+                'Height', obj.Height, 'Show', false, args{:});
+            panel = obj.UIOverlay.UIPanel;
+            refpos = [0 0 obj.Width obj.Height];
+            obj.drawFields(panel);
             cbtn = uibutton(panel, 'Text', 'Cancel', 'ButtonPushedFcn', @obj.close);
             cbtnpos = cbtn.Position;
             cbtnpos(3) = obj.BtnWidth;
-            cbtn.Position = uialign(cbtnpos, refpos, 'right', 'bottom', true, [-5 5]);
-            okbtn = uibutton(panel, 'Text', 'OK', 'ButtonPushedFcn', @(~,~)obj.apply(inp));
+            cbtn.Position = uialign(cbtnpos, refpos, 'right', 'bottom', true, [-7 7]);
+            okbtn = uibutton(panel, 'Text', 'OK', 'ButtonPushedFcn', @(~,~)obj.apply());
             okbtnpos = okbtn.Position;
             okbtnpos(3) = obj.BtnWidth;
             okbtn.Position = uialign(okbtnpos, cbtn, 'right', 'fill', false, [-(obj.BtnWidth+5) 0]);
         end
         
-        function f = addField(obj, parent, title, value)
+        function f = drawFields(obj, parent)
             %% Add field to input
-            f = uieditfield(parent, 'Value', value);
-            f.ValueChangedFcn = @(inp,~)obj.apply(inp);
-            refpos = [0 0 parent.Position([3 4])];
-            inppos = f.Position;
-            inppos(3) = refpos(3) - 5*2;
-            f.Position = uialign(inppos, refpos, 'center', 'center');
-            lbl = uilabel(parent, 'Text', title);
-            lbl.Position(3) = refpos(3) - 5*2;
-            uialign(lbl, refpos, 'center', 'center', true, [0 inppos(4)+5]);
+            refpos = [0 0 obj.Width obj.Height];
+            for i = 1 : height(obj.Fields)
+                title = obj.Fields.title{i};
+                value = obj.Fields.value{i};
+                f = uieditfield(parent, 'Value', value);
+%                 f = uicheckbox(parent);
+                inppos = f.Position;
+                inppos(3) = refpos(3) - 14;
+                inpoffset = [0, i * obj.FieldsGap - 10];
+                f.Position = uialign(inppos, refpos, 'center', 'bottom', false, inpoffset);
+                lbl = uilabel(parent, 'Text', title);
+                lbl.Position(3) = refpos(3) - 5*2;
+                lbloffset = [0, inppos(4) + 0];
+                uialign(lbl, f.Position, 'left', 'center', false, lbloffset);
+                obj.UIFields = [obj.UIFields; f];
+            end
         end
         
         function delete(obj)
             %% Destructor
-            delete(obj.Body);
+            delete(obj.UIOverlay);
         end
         
     end
--- uiinput.m
+++ uiinput.m
@@ -1,7 +1,11 @@
-function [val, ok] = uiinput(varargin)
+function varargout = uiinput(varargin)
 %UIINPUT Summary of this function goes here
 %   Detailed explanation goes here
+showi = cellfun(@(x) (isstring(x)||ischar(x))&&strcmp(x, 'Show'), varargin);
+showi = find(showi);
 i = UI.Input(varargin{:});
-i.show();
-val = i.Value;
-ok = i.Ok;
+if ~isempty(showi) && ~varargin{showi+1}
+    varargout = {i};
+else
+    varargout = {i.Values i.Ok};
+end
