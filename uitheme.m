52869f592295b5a252d41c3e7f76ddc6f8032cab
Pavel Roslovets <roslovets@exponenta.ru>
2019-08-22 18:32:52 +0300

    Optimize uitheme for old releases

--- uitheme.m
+++ uitheme.m
@@ -12,17 +12,17 @@ switch theme
         color = [1 1 1];
         fontcolor = [0.149 0.149 0.149];
     case 'success'
-        color = '#d4edda';
-        fontcolor = '#155724';
+        color = [0.8314 0.9294 0.8549];
+        fontcolor = [0.0824 0.3412 0.1412];
     case 'info'
-        color = '#d1ecf1';
-        fontcolor = '#0c5460';
+        color = [0.8196 0.9255 0.9451];
+        fontcolor = [0.0471 0.3294 0.3765];
     case 'warning'
-        color = '#fff3cd';
-        fontcolor = '#856404';
+        color = [1.0000 0.9529 0.8039];
+        fontcolor = [0.5216 0.3922 0.0157];
     case 'danger'
-        color = '#f8d7da';
-        fontcolor = '#721c24';
+        color = [0.9725 0.8431 0.8549];
+        fontcolor = [0.4471 0.1098 0.1412];
     otherwise
         error('Unknown theme: %s. Use one of the: none|dark|light|success|info|warning|danger', theme);
 end
