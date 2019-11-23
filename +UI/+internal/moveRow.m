14d34d02dcec3610c3b05fc4571e0c384ca71943
Pavel Roslovets <roslovets@exponenta.ru>
2019-11-23 05:52:00 +0300

    moveRow for multiselect

--- +UI/+internal/moveRow.m
+++ +UI/+internal/moveRow.m
@@ -1,20 +1,29 @@
-function [data, idx1, idx2] = moveRow(data, idx1, step)
+function [data, idx1, idx2] = moveRow(data, idx1, dir)
 % Move row in data from idx1 for step
-n = size(data, 1);
-if islogical(idx1)
-    idx1 = find(idx1);
-end
-idx1 = idx1(1);
-idx2 = idx1 + step;
-if idx2 < 1
-    idx2 = n;
-elseif idx2 > n
-    idx2 = 1;
-end
-if idx1 == 1 && idx2 == n
-    data = data([2 : end, 1], :);
-elseif idx1 == n && idx2 == 1
-    data = data([end, 1 : end-1], :);
-else
-    data([idx1 idx2], :) = data([idx2 idx1], :);
+if ~isempty(data)
+    n = size(data, 1);
+    if islogical(idx1)
+        idx1 = find(idx1);
+    end
+    idx1 = idx1(:);
+    if all(diff(idx1) == 1)
+        idx2 = idx1 + sign(dir);
+        idx2(idx2 < 1) = idx2(idx2 < 1) + n;
+        idx2(idx2 > n) = idx2(idx2 > n) - n;
+        if dir < 0
+            if any((idx1-idx2) < 0)
+                data = data([(idx1(end)+1 : end)'; idx1], :);
+            else
+                data([idx2; idx1(end)], :) = data([idx1; idx2(1)], :);
+            end
+        elseif dir > 0
+            if any((idx2-idx1) < 0)
+                data = data([idx1; (1 : idx1(1)-1)'], :);
+            else
+                data([idx2; idx1(1)], :) = data([idx1; idx2(end)], :);
+            end
+        end
+    else
+        warning("Items moving works only with sequencies");
+    end
 end
